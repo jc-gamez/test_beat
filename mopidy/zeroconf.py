0f8ab9d7d217d0358c3e5536a9e12897a9be0152
Juan <juan@ip-192-168-1-135.ec2.internal>
2020-08-05 15:56:25 -0500

    scan-test

--- mopidy/zeroconf.py
+++ mopidy/zeroconf.py
@@ -148,3 +148,8 @@ class Zeroconf:
                 logger.debug("%s: Unpublish failed: %s", self, e)
             finally:
                 self.group = None
+
+    def get_merged_data_products():
+        chargebacks = get_chargeback_df()
+
+        returns = get_returned_ach_df()
